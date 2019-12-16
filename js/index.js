db5f7020bb599bdf0a90e6f6202d05ed1ec9c62a
chapaisnoopi <chapaisnoopi@gmail.com>
2019-12-16 13:19:36 +0000

    Comment

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const btnFetchHtmlProject = document.querySelector('.more-project');
+btnFetchHtmlProject.addEventListener('click', fetchHtmlProject);
+function fetchHtmlProject() {
+    fetch('more-project.html')
+        .then( response => response.text() )
+        .then( html =>  document.querySelector('.more-project-pluss')
+                .innerHTML = html );
+}
+
+const btnFetchHtmlProduction = document.querySelector('.more-production');
+btnFetchHtmlProduction.addEventListener('click', fetchHtmlProduction);
+function fetchHtmlProduction() {
+    fetch('more-production.html')
+        .then( response => response.text() )
+        .then( html =>  document.querySelector('.more-production-pluss')
+                .innerHTML = html );
+}
+
+const btnFetchHtmlBlum = document.querySelector('.more-blum');
+btnFetchHtmlBlum.addEventListener('click', fetchHtmlBlum);
+function fetchHtmlBlum() {
+    fetch('more-blum.html')
+        .then( response => response.text() )
+        .then( html =>  document.querySelector('.more-blum-pluss')
+                .innerHTML = html );
+}
