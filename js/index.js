de00db6743caf74dd50c2563f683b4ec268bf341
chapaisnoopi <chapaisnoopi@gmail.com>
2019-12-20 16:37:15 +0200

    Comment

--- js/index.js
+++ js/index.js
@@ -2,46 +2,6 @@ const clock = document.querySelector('.clock');
 const updateTime = () => clock.innerHTML = new Date().toLocaleTimeString();
 setInterval(updateTime, 1000);
 
-
-// const btnFetchHtmlProject = document.querySelector('.more-project');
-// btnFetchHtmlProject.addEventListener('click', fetchHtmlProject);
-// function fetchHtmlProject() {
-//     fetch('more-project.html')
-//         .then( response => response.text() )
-//         .then( html =>  document.querySelector('.more-project-pluss')
-//                 .innerHTML = html );
-// }
-
-// const btnFetchHtmlProduction = document.querySelector('.more-production');
-// btnFetchHtmlProduction.addEventListener('click', fetchHtmlProduction);
-// function fetchHtmlProduction() {
-//     fetch('more-production.html')
-//         .then( response => response.text() )
-//         .then( html =>  document.querySelector('.more-production-pluss')
-//                 .innerHTML = html );
-// }
-
-// const btnFetchHtmlBlum = document.querySelector('.more-blum');
-// btnFetchHtmlBlum.addEventListener('click', fetchHtmlBlum);
-// function fetchHtmlBlum() {
-//     fetch('more-blum.html')
-//         .then( response => response.text() )
-//         .then( html =>  document.querySelector('.more-blum-pluss')
-//                 .innerHTML = html );
-// }
-
-
-// const btnMoreProduction = document.querySelector('.more-production');
-// btnMoreProduction.addEventListener('click', moreProduction);
-// function moreProduction() {
-//     fetch('more-about.json')
-//         .then( response => response.json() )
-//         .then( data =>  {
-//             document.querySelector('.more-production-pluss').innerHTML = data.moreProduction;
-//         } );
-// }
-
-
 const btnMoreProject = document.querySelector('.more-project');
 btnMoreProject.addEventListener('click', moreProject);
 let idProject = "";
---
+++ js/shop.js
@@ -0,0 +1,5 @@
+const clock = document.querySelector('.clock');
+const updateTime = () => clock.innerHTML = new Date().toLocaleTimeString();
+setInterval(updateTime, 1000);
+
+const 
